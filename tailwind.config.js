e282f51836a631ea15c215e7b79c7f6c98c05342
0xtito <welovethepenguins@gmail.com>
2023-07-12 11:50:31 -0400

    init app configs

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
