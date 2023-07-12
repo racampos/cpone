11651b0d717a326160128bd4b6278e755630fa42
0xtito <welovethepenguins@gmail.com>
2023-07-12 12:03:26 -0400

    update dropbox

--- components/client/DropZone.tsx
+++ components/client/DropZone.tsx
@@ -16,15 +16,12 @@ export default function DropZone() {
       <div className="absolute inset-0 bg-opacity-50 border-2 border-black border-opacity-20 rounded-lg flex items-center justify-center">
         <input {...getInputProps()} />
         {isDragActive ? (
-          <p className="text-black text-center">Drop the files here ...</p>
+          <p className="text-black text-center">Drop your image here ...</p>
         ) : (
           <p className="text-black text-center">
-            Drag 'n' drop some files here, or click to select files
+            Drag 'n' drop image here, or click to select image
           </p>
         )}
-        {/* <p className="text-black text-center">
-          Drag your file here, or click to navigate to it
-        </p> */}
       </div>
       <DropZoneImage />
     </div>
