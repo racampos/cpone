2694b25f869e13f4bd5b23974b2d8ba84ecf5cef
0xtito <welovethepenguins@gmail.com>
2023-07-29 19:21:05 -0400

    updated configs/utils/schema

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDate = (date: string) => {
+  return date
+    .replace(/(?!^)-/g, '')
+    .replace(/(\d{2})/, '$1/')
+    .replace(/(\d{2}\/\d{2})/, '$1/')
+    .slice(0, 10);
+};
