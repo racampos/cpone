e0a638e352d8a92d9ea571c9cc39f7b07e3077a5
0xtito <welovethepenguins@gmail.com>
2023-07-31 15:56:14 -0400

    mina integration

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,7 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true, // need this b/c of babel.config.js
+  reactStrictMode: false,
+  // experimental: {
+  //   forceSwcTransforms: true, // need this b/c of babel.config.js
+  // },
+
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      snarkyjs: require('path').resolve('node_modules/snarkyjs'),
+    };
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
   },
 };
 
